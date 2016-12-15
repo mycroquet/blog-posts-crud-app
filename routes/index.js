fd033aa1e51700d100337f80f3c028390a9b34de
Michael Roque <mikeroque1@gmail.com>
2016-12-15 00:32:16 -0700

    Almost deleting

--- routes/index.js
+++ routes/index.js
@@ -2,56 +2,15 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex')
 
-    /* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res) {
     knex('blog')
-    .select()
-    .then(function(posts) {
-        res.render('index', {
-            posts: posts
+        .select()
+        .then(function(posts) {
+            res.render('index', {
+                posts: posts
+            })
         })
-    })
-})
-
-// CREATE post
-router.post('/create', function(req, res) {
-    knex('blog')
-    .insert({
-        name: req.body.name,
-        date_created: new Date(),
-        title: req.body.title,
-        body: req.body.body
-    })
-    .returning('id')
-    .then(function(data) {
-      res.redirect('/' + data)
-    });
-})
-
-// Gets update page
-router.get('/update/:id', function(req, res){
-  knex('blog')
-  .where('id', req.params.id).first()
-  .then(function(data){
-    res.render('update', {
-        data: data
-    })
-  })
-})
-
-// UPDATE put
-router.post('/update/:id', function(req, res){
-  console.log(req.body);
-  knex('blog')
-  .where('id', req.params.id)
-  .update({
-    name: req.body.name,
-    title: req.body.title,
-    body: req.body.body
-  })
-  .then(function(data) {
-    res.redirect('/' + req.params.id)
-  });
 })
 
 // Gets blog creation page
@@ -59,17 +18,69 @@ router.get('/create', function(req, res) {
     res.render('create')
 })
 
+// CREATE post
+router.post('/create', function(req, res) {
+    knex('blog')
+        .insert({
+            name: req.body.name,
+            date_created: new Date(),
+            title: req.body.title,
+            body: req.body.body
+        })
+        .returning('id')
+        .then(function(data) {
+            res.redirect('/' + data)
+        });
+})
+
+// Gets update page
+router.get('/update/:id', function(req, res) {
+    knex('blog')
+        .where('id', req.params.id)
+        .first()
+        .then(function(data) {
+            res.render('update', {
+                data: data
+            })
+        })
+})
+
+
+// UPDATE post
+router.post('/update/:id', function(req, res) {
+    knex('blog')
+        .where('id', req.params.id)
+        .update({
+            name: req.body.name,
+            title: req.body.title,
+            body: req.body.body
+        })
+        .then(function(data) {
+            res.redirect('/' + req.params.id)
+        });
+})
+
+
 // Gets blog post page
 router.get('/:id', function(req, res) {
     knex('blog')
-    .where('id', req.params.id)
-    .first()
-    .then(function(posts) {
-        res.render('details', {
-            posts: posts
+        .where('id', req.params.id)
+        .first()
+        .then(function(posts) {
+            res.render('details', {
+                posts: posts
+            })
         })
-    })
 })
 
+// deletes blog
+router.delete('/:id', function(req, res) {
+    knex('blog')
+        .where('id', req.params.id)
+        .del()
+        .then(function() {
+            res.redirect('/')
+        });
+})
 
 module.exports = router;
